c62f4a00bc43b04b855b3de1ca66c774ab897b8b
naker90 <AntonioJesusSG96@gmail.com>
2017-10-10 20:29:15 +0100

    refactor - delete switch statement and unused method

--- javascript/src/solution/GildedRoseSwitch.js
+++ javascript/src/solution/GildedRoseSwitch.js
@@ -1,27 +1,10 @@
 GildedRoseSwitch = function(items){
-
     this.updateQuality =  function() {
         for (let index in items) {
-            let item = items[index];
-
-            switch (item.getName()){
-                default:
-                    item.updateQuality();
-                    break;
-            }
-            item.setSellIn(item.getSellIn()-1);
+            items[index].updateQuality();
+            items[index].setSellIn(items[index].getSellIn()-1);
         }
     };
-
-
-    function updateItemGenericQuality(item) {
-        if(item.getQuality() > 0){
-            if(item.getSellIn() <= 0){
-                item.setQuality(item.getQuality()-2);
-            }else{item.setQuality(item.getQuality()-1);}
-        }
-    }
-
 };
 
 module.exports = GildedRoseSwitch;
